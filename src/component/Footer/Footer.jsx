a253224e9be66132d49358b728acf9498a706a94
Humayra-Amin <humaema4576@gmail.com>
2024-08-17 01:59:23 +0600

    footer added

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from '../../assets/images/logo.png'
+import { FaFacebook, FaInstagram,FaGoogle } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer footer-center bg-blue-300 text-primary-content p-10">
+                <aside>
+                <img src={logo} alt="" className='h-20'/>
+
+                    <p className="font-bold text-black leading-9">
+                        Filter Mart Ltd.
+                        <br />
+                        Providing reliable Products
+                    </p>
+                    
+                </aside>
+                <nav>
+                    <div className="grid grid-flow-col gap-4">
+                        <a className='border bg-blue-500 p-2 rounded-lg border-blue-500 text-2xl hover:cursor-pointer'><FaFacebook></FaFacebook></a>
+                        <a className='border bg-blue-500 p-2 rounded-lg border-blue-500 text-2xl hover:cursor-pointer'><FaGoogle></FaGoogle></a>
+                        <a className='border bg-blue-500 p-2 rounded-lg border-blue-500 text-2xl hover:cursor-pointer'><FaInstagram></FaInstagram></a>
+                    </div>
+                </nav>
+
+                <div>
+                <p className='text-black'>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
